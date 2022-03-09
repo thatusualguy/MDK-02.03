dc63d863b115f54fe0f5dada7dbf764fd43c07c2
Максим Казанцев <m.p.rybakov@gmail.com>
2022-03-09 15:47:04 +0300

    changed to static port

--- AlpsBottleWebProject/app.py
+++ AlpsBottleWebProject/app.py
@@ -24,9 +24,9 @@ if __name__ == '__main__':
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+        PORT = int(os.environ.get('SERVER_PORT', '55550'))
     except ValueError:
-        PORT = 5555
+        PORT = 55550
 
     @bottle.route('/static/<filepath:path>')
     def server_static(filepath):
